670824157d87c26be3e18a07661cd308938beff4
labib08 <iklabib49@gmail.com>
2023-09-23 15:37:36 +1000

    Initial Push

---
+++ ArtistList.py
@@ -0,0 +1,55 @@
+import json
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+
+    html_text = requests.get("https://www.insider.com/spotify-most-monthly-listeners-artists-2023-8").text
+    soup = BeautifulSoup(html_text, 'lxml')
+    artist_dict = {}
+    ranking = 20
+    artists = soup.find_all('div', class_ = 'slide keyline')
+
+    for artist in artists:
+        artist_info = []
+
+        artist_name = artist.find('h2', class_ = 'slide-title-text').text
+        artist_name = re.search(r'\d+\.\s*(.+)', artist_name).group(1)
+        artist_info.append(artist_name)
+
+        artist_listeners= artist.find_all('p')[0].text
+        artist_listeners = re.search(r'(\d+(?:\.\d+)?)\s*million', artist_listeners).group()
+        artist_info.append(artist_listeners)
+
+        most_streamed_song = artist.find_all('p')[1].text
+        pattern = r"Most streamed song:\s+(.+)"
+        artist_song = re.search(pattern, most_streamed_song).group(1)
+        artist_info.append(artist_song)
+
+        artist_image = artist.find('div', class_ = 'lazy-holder').find('noscript').find('img').get('src')
+        artist_info.append(artist_image)
+
+        artist_dict[ranking] = artist_info
+        ranking -= 1
+
+
+    artist_dict = dict(sorted(artist_dict.items()))
+
+    with open("sample.json", "w") as outfile:
+        json.dump(artist_dict, outfile)
+
+    json_object = json.dumps(artist_dict)
+    with open("Top Artists", "w") as outfile:
+        outfile.write(json_object)
+
+    """
+    for key, value in artist_dict.items():
+        print(f"Ranking: {key} Artist: {value[0]}, Monthly listeners {value[1]}, Most streamed song {value[2]} Looks: {value[3]}.")
+        print("")
+    """
+
+if __name__ == "__main__":
+    main()
